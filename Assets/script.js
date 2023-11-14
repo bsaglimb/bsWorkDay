672a17113d1a9b361846ccf167efd244fdefa799
bsaglimb <bsaglimb>
2023-11-13 19:58:40 -0500

    added to the script file

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,20 @@
 
+const localeSettings = {};
+dayjs.locale(localeSettings);
+  
+
+$(function () {
+
+  const currentHour = dayjs().format('H');
+  function hourlyColor() {
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      $(this).toggleClass('past', blockHour < currentHour);
+      $(this).toggleClass('present', blockHour === currentHour);
+      $(this).toggleClass('future', blockHour > currentHour);
+    });
+  }
+})
 
 
  
