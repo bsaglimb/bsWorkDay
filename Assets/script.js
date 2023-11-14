7bb0a889671521ed39e68b2eb26b567faa10c487
bsaglimb <bsaglimb>
2023-11-13 20:22:00 -0500

    added the rest of the js code

--- Assets/script.js
+++ Assets/script.js
@@ -14,7 +14,49 @@ $(function () {
       $(this).toggleClass('future', blockHour > currentHour);
     });
   }
-})
+
+  function textEntry() {
+    $('.saveBtn').on('click', function() {
+      const key = $(this).parent().attr('id');
+      const value = $(this).siblings('.description').val();
+      localStorage.setItem(key, value);
+    });
+  }
+
+  function refreshColor() {
+    $('.time-block').each(function() {
+      const blockHour = parseInt(this.id);
+      if (blockHour == currentHour) {
+        $(this).removeClass('past future').addClass('present');
+      } else if (blockHour < currentHour) {
+        $(this).removeClass('future present').addClass('past');
+      } else {
+        $(this).removeClass('past present').addClass('future');
+      }
+    });
+  }
+
+  $('.time block').each(function() {
+    var key = $(this).attr('id');
+    var value = localStorage.getItem(key);
+    $(this).children('.description').val(value);
+  });
+
+  function updateTime() {
+    var dateElement = $('#date');
+    var timeElement = $('#time');
+    var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+    var currentTime = dayjs().format('hh:mm:ss A');
+    dateElement.text(currentDate);
+    timeElement.text(currentTime);
+  }
+  
+  hourlyColor();
+  textEntry();                
+  refreshColor();
+  setInterval(updateTime, 1000);
+});
+
 
 
  
